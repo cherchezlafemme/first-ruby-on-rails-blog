42f235260cb62a7c455b01cab3e9219b22a0d4d3
Anna Krupp <anyakrupp@gmail.com>
2016-07-31 18:29:53 -0700

    Add functionality to delete existing comments, add link to the dependency of the comments to the article. So when you delete the article all related to it comments are deleting as well

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
-
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
@@ -10,5 +16,4 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
-
 end
